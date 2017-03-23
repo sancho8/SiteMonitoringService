18ab95c2db812c0b68e263571955d56c5497ee63
sancho8 <98sancho@ukr.net>
2017-03-23 23:12:03 +0200

    added Quartz, Owin, added ApiController with CheckStatus method

---
+++ SiteMonitoringService/StatusController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SiteMonitoringService
+{
+    class StatusController: ApiController
+    {
+        public bool CheckStatus()
+        {
+            ServiceController sc = new ServiceController("MyTestService");
+            return (sc.Status == ServiceControllerStatus.Running);
+        }
+    }
+}
